a9c65c9017b5f1efce96193baa536e127da33f55
Pavel Kondratjonok <pavel.kondratjonok@fingers.by>
2021-11-16 17:36:32 +0300

    test

--- pavel_dag.py
+++ pavel_dag.py
@@ -360,7 +360,7 @@ with DAG(
                 #     # TaskGroup level dependencies
                 #     create_temp_table >> parquet_all_raitings >> parquet_scores >> parquet_reviews >> parquet_product_scores >> remove_temp_table
         # start >> dynamic_tasks_group_load
-
+        start
         return dag_subdag
 
     load_tasks = SubDagOperator(
