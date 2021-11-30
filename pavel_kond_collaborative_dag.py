a05dbf57c7ff65522f6aaf1f59d216aed9c29b14
Pavel Kondratjonok <pavel.kondratjonok@fingers.by>
2021-11-30 23:02:13 +0300

    add collaborative

--- pavel_kond_collaborative_dag.py
+++ pavel_kond_collaborative_dag.py
@@ -12,5 +12,5 @@ with DAG(
 
     bash_run_pyspark_task = BashOperator(
         task_id='bash_run_pyspark_task',
-        bash_command= '''cp /tmp/id_rsa_shahidkubik /tmp/id_rsa_shahidkubik_tmp && chmod 600 /tmp/id_rsa_shahidkubik_tmp && ssh -i /tmp/id_rsa_shahidkubik_tmp shahidkubik@rc1b-dataproc-m-h8jzox1botuktl9j.mdb.yandexcloud.net spark-submit --master yarn --deploy-mode cluster --conf spark.sql.catalogImplementation=hive /home/shahidkubik/pavel_kond_collaborative.py'''
+        bash_command= '''cp /tmp/id_rsa_shahidkubik /tmp/id_rsa_shahidkubik_tmp && chmod 600 /tmp/id_rsa_shahidkubik_tmp && ssh -i /tmp/id_rsa_shahidkubik_tmp shahidkubik@rc1b-dataproc-m-h8jzox1botuktl9j.mdb.yandexcloud.net spark-submit --master yarn --deploy-mode client --conf spark.sql.catalogImplementation=hive /home/shahidkubik/pavel_kond_collaborative.py'''
     )
